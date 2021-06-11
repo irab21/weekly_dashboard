2cc5d8d4d7cc77779d05dc6b95286a7b75988a90
irab21 <65721379+irab21@users.noreply.github.com>
2021-06-11 12:17:22 +0530

    Update code.py

--- code.py
+++ code.py
@@ -94,7 +94,7 @@ st.sidebar.markdown('### Scaled Ratio Score')
 if st.sidebar.checkbox('CV Ratio Scale',True,key=4):
 	st.write('### CV Ratio Scale is the ratio of cv submitted to the cv submission target scaled ')
 	fig3=px.area(x=team1,y=cv_sr,text=cv_sr)
-	fig3.update_traces(texttemplate='%{text:.2s}',textposition='top center',width=0.4)
+	fig3.update_traces(texttemplate='%{text:.2s}',textposition='right center')
 	st.plotly_chart(fig3)
 
 
