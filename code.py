fca03b5a44bc31fbee03a54cfc3595d1dfea07a8
irab21 <65721379+irab21@users.noreply.github.com>
2021-06-11 16:31:44 +0530

    Update code.py

--- code.py
+++ code.py
@@ -109,13 +109,13 @@ st.sidebar.markdown('### Final Scores')
 
 
 final_score=data['Final Point Scale'].to_list()
-final_score=final_score.sort()
+final_score=final_score
 final_score=pd.DataFrame({'Team':team1,'score':final_score.values})
 st.write(final_score)
 
 if st.sidebar.checkbox('Final Score',True,key=6):
 	st.write('### This is the final score calculated out of 10,arranged in ascending order')
-	fig5=px.funnel(final_score,x='score',y='Team',text='score',orientation='h',height=600)
+	fig5=px.area(final_score,x='score',y='Team',text='score',height=600)
 	fig5.update_traces(texttemplate='%{text:.2s}')
 	st.plotly_chart(fig5)
 
