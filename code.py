083ea39de48aebcc8e6f2274355cd357d7f771ed
irab21 <65721379+irab21@users.noreply.github.com>
2021-06-11 13:20:58 +0530

    Update code.py

--- code.py
+++ code.py
@@ -102,7 +102,7 @@ if st.sidebar.checkbox('CV Ratio Scale',True,key=4):
 if st.sidebar.checkbox('Interview Ratio Scale',True,key=5):
 	st.write('### Interview Ratio scale is a scaled ratio of Total Number of Interviews to the Total Number of CV Submitted')
 	st.write('\n\n')
-	fig4=px.line(x=team1,y=i_sr,text=i_sr)
+	fig4=px.line(x=team1,y=[1.21,0.78,1.803,5.06,4.17,2.17,0.833,3.469],text=i_sr)
 	fig4.update_traces(texttemplate='%{text:.2s}',textposition='top center')
 	st.plotly_chart(fig4)
 
