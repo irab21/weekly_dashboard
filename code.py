e7723c20db1ccc42559f2321fd896b7aa9a5723b
irab21 <65721379+irab21@users.noreply.github.com>
2021-06-11 15:44:42 +0530

    Update code.py

--- code.py
+++ code.py
@@ -110,7 +110,7 @@ st.sidebar.markdown('### Final Scores')
 
 final_score=data['Final Point Scale'].to_list()
 final_score=final_score.sort()
-final_score=pd.DataFrame({'Team':team1,'score':final_score})
+final_score=pd.DataFrame({'Team':team1,'score':final_score.values})
 st.write(final_score)
 
 if st.sidebar.checkbox('Final Score',True,key=6):
